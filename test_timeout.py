0e002a5f05a7c2c2a1877c8f30607026be7869c4
Eli Uriegas <seemethere101@gmail.com>
2016-12-06 22:25:45 -0600

    Test datetime matches up

--- test_timeout.py
+++ test_timeout.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from time import sleep
 
 import pytest
@@ -5,9 +6,13 @@ import timeout
 
 
 def test_timeout_not_reached():
+    """Test when a timeout is not reached"""
+    timeout_time = 3
+    sleep_time = 2
     try:
-        with timeout.of(3):
-            sleep(2)
+        with timeout.of(timeout_time) as then:
+            sleep(sleep_time)
+        assert (datetime.now() - then).seconds == sleep_time
         # We sleep after the timeout has ended to make sure there's no
         # lasting signal calls
         sleep(2)
@@ -17,18 +22,24 @@ def test_timeout_not_reached():
 
 
 def test_timeout_reached():
+    """Test when a timeout is reached"""
+    timeout_time = 3
+    sleep_time = 4
     with pytest.raises(timeout.MaximumTimeoutExceeded):
-        with timeout.of(3):
-            sleep(4)
+        with timeout.of(timeout_time) as then:
+            sleep(sleep_time)
+    assert (datetime.now() - then).seconds == timeout_time
 
 
 def test_func_timeout_not_reached():
+    """Test when a decorated function's timeout is not reached"""
     @timeout.after(3)
     def func():
         sleep(2)
-
     try:
         func()
+        # We sleep after the timeout has ended to make sure there's no
+        # lasting signal calls
         sleep(2)
     except timeout.MaximumTimeoutExceeded:
         pytest.fail(
@@ -36,9 +47,9 @@ def test_func_timeout_not_reached():
 
 
 def test_func_timeout_reached():
+    """Test when a decorated function's timeout is reached"""
     @timeout.after(3)
     def func():
         sleep(4)
-
     with pytest.raises(timeout.MaximumTimeoutExceeded):
         func()
